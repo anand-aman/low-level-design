a1a023bc48939ba10733d239bb8df9189b04cb34
anand-aman <itsamananand@gmail.com>
2026-03-08 12:39:14 +0530

    tic tac toe refactor

--- 4-tic-tac-toe/src/main/java/com/lld/tictactoe/TicTacToe.java
+++ 4-tic-tac-toe/src/main/java/com/lld/tictactoe/TicTacToe.java
@@ -1,6 +1,5 @@
 package com.lld.tictactoe;
 
-import com.lld.tictactoe.model.Board;
 import com.lld.tictactoe.model.PieceType;
 import com.lld.tictactoe.model.Player;
 import com.lld.tictactoe.model.PlayingPiece;
@@ -46,62 +45,16 @@ public class TicTacToe {
             else {
                 board.getBoard()[row][col] = player.getPlayingPiece();
                 board.printBoard();
-                if (isWinner(player, row, col)) {
+                if (board.isWinner(player, row, col)) {
                     System.out.println("Player " + player.getName() + " wins!");
                     nowWinner = false;
                     continue;
                 }
                 players.addLast(player);
             }
-
         }
-
 
     }
-
-
-    private boolean isWinner(Player player, int row, int column) {
-
-        PlayingPiece[][] gameBoard = board.getBoard();
-        PieceType pieceType = player.getPlayingPiece().getPieceType();
-
-        boolean rowMatch = true;
-        boolean columnMatch = true;
-        boolean diagonalMatch = true;
-        boolean antiDiagonalMatch = true;
-
-        //need to check in row
-        for(int i=0; i<board.getSize(); i++) {
-            if(gameBoard[row][i] == null || gameBoard[row][i].getPieceType() != pieceType) {
-                rowMatch = false;
-            }
-        }
-
-        //need to check in column
-        for(int i=0; i<board.getSize(); i++) {
-
-            if(gameBoard[i][column] == null || gameBoard[i][column].getPieceType() != pieceType) {
-                columnMatch = false;
-            }
-        }
-
-        //need to check diagonals
-        for(int i=0, j=0; i<board.getSize(); i++,j++) {
-            if (gameBoard[i][j] == null || gameBoard[i][j].getPieceType() != pieceType) {
-                diagonalMatch = false;
-            }
-        }
-
-        //need to check anti-diagonals
-        for(int i=0, j=board.getSize()-1; i<board.getSize(); i++,j--) {
-            if (gameBoard[i][j] == null || gameBoard[i][j].getPieceType() != pieceType) {
-                antiDiagonalMatch = false;
-            }
-        }
-
-        return rowMatch || columnMatch || diagonalMatch || antiDiagonalMatch;
-    }
-
 
     private PieceType takeSymbolInput(int i) {
         System.out.print("Enter piece type (X/O) for Player " + i + " : ");
