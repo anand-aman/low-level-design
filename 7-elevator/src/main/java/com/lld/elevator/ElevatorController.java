5daceb0ecca390c986902ec7d9db6a4187aacda5
anand-aman <itsamananand@gmail.com>
2026-03-21 16:39:53 +0530

    elevator lld - initial

---
+++ 7-elevator/src/main/java/com/lld/elevator/ElevatorController.java
@@ -0,0 +1,16 @@
+package com.lld.elevator;
+
+import java.util.List;
+
+public class ElevatorController {
+    List<Elevator> elevators;
+
+    Elevator requestElevator(int floor, Direction direction) {
+        return null;
+    }
+
+    void step() {
+
+    }
+
+}
---
+++ 7-elevator/src/main/java/com/lld/elevator/Requests.java
@@ -0,0 +1,7 @@
+package com.lld.elevator;
+
+public class Requests {
+    int floor;
+    RequestType requestType;
+}
+
